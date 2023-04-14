35decb25184b589882e25f13dd6a2e92f6393143
IngMollete <fabianwhijares@hotmail.com>
2023-04-14 03:21:02 -0600

    Funciones en JavaScript

--- JS3.js
+++ JS3.js
@@ -1,21 +1,28 @@
-//Funciones declarativas:
-function miPrimerafuncion(){
+//Funciones: En JavaScript tenemos 2 tipos de funciones:
 
+
+//Funciones Declarativas: En las funciones declarativas, 
+//utilizamos la palabra reservada "function" al inicio 
+//para poder declarar la funcion: 
+
+function miPrimerafuncion(){
     return 3;
 }
 
 miPrimerafuncion();
 
-//Funciones expresivas:
-var miPrimerafuncion = function(a,b){
+//Funciones anonimas: En la funcion anonima, la 
+//declaracion se incia con la palabra reservada "var"
+//donde se generara una variable que guardara 
+//una funcion anonima.
 
-    return a + b;
-
+var nombre = function(nombre){
+    console.log(`Hola ${nombre}`)
 }
 
-miPrimerafuncion();
+nombre("Mollete");
 
-
+//Otros ejemplos de funciones:
 function saludoEstudiante(Estudiante) {
     console.log(`Hola ${Estudiante}`);
 }
@@ -24,3 +31,9 @@ function sumar(a,b) {
     var resultado = a+b;
      return resultado;
  }
+
+ //La diferencia entre las Funciones declarativas y las 
+ //Funciones anonimas es que a las funciones declarativas
+ //se le aplica hoisting (Revisar JS5.js), y a la 
+ //funcion anonima no, ya que el hoisting solo se aplica
+ //en las palaras reservadas var y function. 
