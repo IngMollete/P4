5386366e1875500ff4bc2460d89076a38665ada6
IngMollete <fabianwhijares@hotmail.com>
2023-04-14 03:20:32 -0600

    Variables JavaScript

--- JS2.js
+++ JS2.js
@@ -1,16 +1,16 @@
-var nombre = "Fabian";
-var concha
-var personas
-
-
 // Declarar una variable
 var edad;
 //Inicializar una variable
 edad = 20;
 
-personas = ["Pedro","Francisco"]
+//Las variables se pueden declarar e inicializar en una misma 
+//linea de texto: 
+var edad = 20; //Una variable tipo Number
+var nombre = "Mollete"; //Una variable tipo String
+var vof = true; //Una variable tipo Booleano 
+personas = ["Pedro","Francisco"] //Una variable tipo Array
 concha = {
     nombre: "Jose",
-    edad: 15
-}
+    edad: 15    
+    } //Una variable tipo Objeto
 
